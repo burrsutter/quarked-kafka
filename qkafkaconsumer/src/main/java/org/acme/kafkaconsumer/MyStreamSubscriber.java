0312d1eb01dc5ae8c3a26e1e325df78ca7da6321
Burr Sutter <burrsutter@gmail.com>
2019-06-18 19:11:24 -0400

    adding a real transformation example

--- qkafkaconsumer/src/main/java/org/acme/kafkaconsumer/MyStreamSubscriber.java
+++ qkafkaconsumer/src/main/java/org/acme/kafkaconsumer/MyStreamSubscriber.java
@@ -1,16 +1,26 @@
 package org.acme.kafkaconsumer;
 
+import java.io.StringReader;
+import java.math.BigDecimal;
+
 import javax.enterprise.context.ApplicationScoped;
+import javax.json.Json;
+import javax.json.JsonValue;
+import javax.json.JsonArray;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonNumber;
 import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
+import javax.json.JsonReader;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
-import org.eclipse.microprofile.metrics.annotation.Counted;
+// import org.eclipse.microprofile.metrics.annotation.Counted;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.eclipse.microprofile.reactive.messaging.Outgoing;
 import org.jboss.logging.Logger;
 
-import io.reactivex.Flowable;
 
 @ApplicationScoped
 @Path("/")
@@ -20,45 +30,130 @@ public class MyStreamSubscriber {
     @GET
     @Path("reset")
     public String reset() {
-      return "This is here to cause live reload";
+      return "This is here to cause live reload: curl localhost:8083/reset";
     }
  
     // @Incoming("input")
     // public void process(String msg) {
-    //     LOG.info("input1: " + msg);
+    //     LOG.info("INPUT1: " + msg);
     // }     
 
     // @Incoming("input")
     // public CompletionStage<Void> process(KafkaMessage<String,String> msg) {
-    //     LOG.info("input2: " + msg.getPayload());
+    //     LOG.info("INPUT2: " + msg.getPayload());
     //     return msg.ack();
     // }
 
     // you see input3 logged 1 time
+
     // @Incoming("input")
     // @Outgoing("output")
-    // public Flowable<JsonObject> process(Flowable<JsonObject> input) {      
-    //   System.out.println("\n\n\ninput3: " + input.toString());
+    // public Flowable<String> process(Flowable<String> input) {      
+    //   System.out.println("\n\n\nINPUT3: " + input.toString());
     //   return input;
     // }
 
-    @Incoming("input")
-    @Outgoing("output")
-    public Flowable<JsonObject> process(Flowable<JsonObject> input) {      
-      // return input;
-      return input
-        .doOnError(e -> System.out.println("ERROR: " + e + "\n"))
-        .doOnNext(json -> System.out.println("INPUT4: " + json + "\n"))
-        ;
-    }
+    // Simple mapping of input to output
 
     // @Incoming("input")
     // @Outgoing("output")
-    // public JsonObject process(JsonObject input) {
-    //   LOG.info("input3: " + input);
+    // public String process(String input) {
+    //   LOG.info("INPUT4: " + input);
     //   return input;
     // }
     
+    // Allows you to see any errors
+
+    // @Incoming("input")
+    // @Outgoing("output")
+    // public Flowable<String> process(Flowable<String> input) {      
+    //   // return input;
+    //   return input        
+    //     .doOnNext(json -> System.out.println("INPUT5: " + json + "\n"))
+    //     .doOnError(e -> System.out.println("ERROR: " + e + "\n"))
+    //     ;
+    // }
+
+    // Add a Customer Name to the output message
+    // @Incoming("input")
+    // @Outgoing("output")
+    // public String transform(String input) {
+    //   LOG.info("INPUT6: " + input);
+
+    //   JsonReader jsonReader = Json.createReader(new StringReader(input));
+    //   JsonObject myJsonObject = jsonReader.readObject();   
+    
+    //   // now rebuild the jsonobject, with the new entry
+    //   JsonObjectBuilder builder = Json.createObjectBuilder();   
+    //   builder.add("custName", "Special K");
+    //   myJsonObject.entrySet().forEach(e -> builder.add(e.getKey(),e.getValue()));
+    //   JsonObject newJson = builder.build();
+      
+    //   String newJsonAsString = newJson.toString();
+    //   LOG.info("OUTPUT6: " + newJsonAsString);
+
+    //   return newJsonAsString;
+    // }    
+    
+    
+    @Incoming("input")
+    @Outgoing("output")
+    public String transform(String input) {
+      LOG.info("INPUT7: " + input);
+
+      JsonReader jsonReader = Json.createReader(new StringReader(input));
+      JsonObject myJsonObject = jsonReader.readObject();   
+    
+      
+      JsonObjectBuilder mainBuilder = Json.createObjectBuilder();   
+      mainBuilder.add("id",myJsonObject.getString("id"));
+      // new classification
+      mainBuilder.add("class", "Special K Customer");
+
+      JsonArrayBuilder orderArrayBuilder = Json.createArrayBuilder();
+      JsonObjectBuilder orderBuilder = Json.createObjectBuilder();
+            
+
+      JsonArray theOrders = myJsonObject.getJsonArray("orders");
+      LOG.info("theOrders: " + theOrders);
+      
+      double orderTotal = 0;
+
+      for(int i= 0; i < theOrders.size(); i++) {
+        JsonValue item = theOrders.get(i);
+        int qty = item.asJsonObject().getInt("qty");
+        System.out.print("qty:" + qty);
+        JsonNumber priceAsJson = item.asJsonObject().getJsonNumber("price");
+        BigDecimal price = priceAsJson.bigDecimalValue();
+        System.out.println(" price: " + price);
+        orderBuilder
+          .add("id", item.asJsonObject().getInt("id"))
+          .add("itemid", item.asJsonObject().getString("itemid"))
+          .add("description", item.asJsonObject().getString("description"))
+          .add("qty", item.asJsonObject().getInt("qty"))
+          .add("price",item.asJsonObject().getJsonNumber("price"));
+          // new extended price
+          double extendedPrice = qty * price.doubleValue();
+        orderBuilder  
+          .add("extended", extendedPrice);
+      
+        orderArrayBuilder.add(orderBuilder.build());
+        orderTotal = orderTotal + extendedPrice;
+        
+      }
+      
+      LOG.info("TWO");
+      
+      mainBuilder.add("orderTotal", orderTotal);
+      mainBuilder.add("orders",orderArrayBuilder.build());
+
+      JsonObject newJson = mainBuilder.build();
+      
+      String newJsonAsString = newJson.toString();
+      LOG.info("OUTPUT7: " + newJsonAsString);
+
+      return newJsonAsString;
+    }    
 
 }
 
